116facf77d2b1a74d4fedf066866f96a624b8d18
jujaeyoung <jujy4315@gmail.com>
2022-03-16 11:40:39 +0900

    변수 선언

---
+++ 롯데월드.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int firstTicket, secondTicket;
+	int dayTime, nightTime;
+	int age;
+	int 
+	
+	printf("권종을 입력하세요. :");
+	scanf("%d %d", &dayTime, &nightTime);
+	printf("주민번호를 입력하세요. :");
+	scanf("%d", &age);
+	 
+	
+	
+	
+	return 0;
+ } 
